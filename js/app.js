df760cceaa5ba68a5f540875416570a15798bbef
Jeremy <jeremy.benoist@gmail.com>
2013-04-28 12:40:29 +0200

    Add search engine
    Using lunr.
    Inspired from http://dreamand.me/web/fulltext-search-at-jekyll-site/ & https://github.com/slashdotdash/jekyll-lunr-js-search

--- js/app.js
+++ js/app.js
@@ -1,20 +1,47 @@
-$(function() {
-  $("#search button").click(function() {
-    search();
-  });
-  $("#search input").keypress(function(e) {
-    if(e.which == 13) {
-      e.preventDefault();
-      search();
-    }
-  });
-});
 
-function search() {
-  var result = idx.search($("#search input").val());
-  if(result && result.length > 0) {
-    window.location.replace(result[0].ref);
-  } else {
-    alert("Found nothing");
+$(function() {
+  $('#search input.button').on('click', function() {
+    return search();
+  });
+
+  $('#search-query').on('keyup', function() {
+    return search();
+  });
+
+  function search() {
+    var query   = $('#search-query').val();
+    var result  = $('#search-results');
+    var entries = $('#search-results .entries');
+
+    if (query.length <= 2) {
+      result.hide();
+      entries.empty();
+    } else {
+      // retrieve matching result with content
+      var results = $.map(idx.search(query), function(result) {
+        return $.grep(docs, function(entry) {
+          return entry.id === result.ref;
+        })[0];
+      });
+
+      entries.empty();
+
+      if (results && results.length > 0) {
+        $.each(results, function(key, post) {
+          entries.append('<article>'+
+          '  <h3>'+
+          '    <small><time>'+post.date+'</time></small>'+
+          '    <a href="'+post.id+'">'+post.title+'</a>'+
+          '  </h3>'+
+          '</article>');
+        });
+      } else {
+        entries.append('<p>Aucun article trouvé :-(</p>');
+      }
+
+      result.show();
+    }
+
+    return false;
   }
-}
+});
