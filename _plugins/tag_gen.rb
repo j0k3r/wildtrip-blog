2fa0faf59099e176d2d49f9ea3c764cae95e5c00
Jeremy Benoist <jeremy.benoist@gmail.com>
2017-01-17 18:13:28 +0100

    Fix tag url generation in sitemap
    And exclude 404 from the sitemap

--- _plugins/tag_gen.rb
+++ _plugins/tag_gen.rb
@@ -1,17 +1,22 @@
 module Jekyll
 
   class TagIndex < Page
-    def initialize(site, base, dir, tag)
+    def initialize(site, base, dir, tag, path)
       @site = site
       @base = base
       @dir = dir
-      @name = tag.delete(' ') + '.html'
+      @name = path
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
+
       self.data['tag'] = tag
+      prettytag = tag.gsub(/(\w+)/) {|s| s.capitalize}
+
+      self.data['permalink'] = '/'+dir+'/'+path
+
       tag_title_prefix = site.config['tag_title_prefix'] || 'Posts Tagged &ldquo;'
       tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;'
-      prettytag = tag.gsub(/(\w+)/) {|s| s.capitalize}
+
       self.data['title'] = "#{tag_title_prefix}#{prettytag}#{tag_title_suffix}"
 
       # if this tag has associated location (defined in _config.yml), add it
@@ -26,6 +31,7 @@ module Jekyll
     def generate(site)
       if site.layouts.key? 'tag_index'
         dir = site.config['tag_dir'] || 'tag'
+
         site.tags.keys.each do |tag|
           write_tag_index(site, dir, tag)
         end
@@ -33,9 +39,12 @@ module Jekyll
     end
 
     def write_tag_index(site, dir, tag)
-      index = TagIndex.new(site, site.source, dir, tag)
+      path = tag.delete(' ') + '.html'
+
+      index = TagIndex.new(site, site.source, dir, tag, path)
       index.render(site.layouts, site.site_payload)
       index.write(site.dest)
+
       site.pages << index
     end
   end
